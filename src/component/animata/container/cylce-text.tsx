cfee4d8636a5cea0e324e4c5b2e70d8d8c8d04f5
Anthony <138202547+toney010319@users.noreply.github.com>
2026-09-14 02:42:28 +0800

    update cycle text

--- src/component/animata/container/cylce-text.tsx
+++ src/component/animata/container/cylce-text.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function CycleText() {
-  const words = ["Front-End Developer", "Back-End Developer", "Full-Stack Developer", "UI/UX Developer"];
+  const words = ["Frontend Engineer", "Full-Stack Web Developer", "Web Developer", ];
   const [index, setIndex] = useState(0);
 
   const total = words.length;
