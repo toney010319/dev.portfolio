8258e393751cfcfdd4f34832d4c3a3400fc238a0
Anthony <138202547+toney010319@users.noreply.github.com>
2026-09-18 05:32:59 +0800

    fix overlay

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="fixed left-0 right-0 top-0 z-50 bg-[#10131A]">
+    <nav className="fixed left-0 right-0 top-0 z-50 bg-[#10131A] pr-[var(--scroll-lock-pad,0px)]">
       <div className="container mx-auto px-4">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
